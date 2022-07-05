d473009b782e6ed13055bffb760e32034c260ee3
Paul Ibeabuchi <ibeabuchi@worklio.cz>
2022-07-05 15:52:00 +0100

    added all wishes data

---
+++ src/allWishes.js
@@ -0,0 +1,78 @@
+const wishes = [
+    {
+        msg: `Ayomide, you've been nothing but an amazing brother ever since we met! I'm so proud of you, this is only the beginning for you!`,
+        author: "Ayomide Makinde"
+    },
+    {
+        msg: `Well what can I say Ayo my name sake is a wonderful person always tries to come through for his people, Guys like Ayo are hard to find. Thanks for all the advice and help bro. Love you brother.
+        But Fr why did you copy my twitter handle ffs be original you thief ass nigga.`,
+        author: "Love, Ekoko"
+    },
+    {
+        msg: `Congratulations on this amazing feat - I know more than most what you had to overcome in bagging this degree, so I am most proudest of you. Love you so much bro, you dey motivate me aje 😅`,
+        author: "Aisha"
+    },
+    {
+        msg: `Congratulations brother. Super proud of you.
+        Now wey masters don finish, make we jam for cockpit.
+        You deserve all the roses and I'm Super happy for you captain.`,
+        author: "Flojo"
+    },
+    {
+        msg: `Dear Ayo,
+        I know we don’t talk often as much as it should be but you’re always on my mind. You’re a good friend with a pure heart, you look out for the ones you really care about and I really like that about you.
+        I remember when the journey started from you gaining admission and relocating to the UK. You really worked hard and now you deserve to be celebrated. Congratulations Ayo, you’ve made me and everyone else proud🤗. This definitely wouldn’t be the last time I get to brag about you 🏆❤️`,
+        author: "Dolly Sho"
+    },
+    {
+        msg: `Alabi Ayomide
+        Its your graduation and I’m excited that i have the opportunity to make it a memorable day for you. 
+        When we met in 200 level, I was going to write you off as one of them play boys. Ayomide you proved to me that our friendship was more than that. In your little way, you tried to make sure I was doing good academically and mentally too. 
+        In our 400 level when everyone had gone far with their projects and I hadn’t even done much, you made it a duty of yours to help me. Not sure you remember but you actually dedicated a day to review what I had done , check for plagiarism (first time I actually knew there was a site to check that) and gave me materials to help me forge forward. 
+        You’ve been a sweet teddy bear of a friend. You are indeed a true friend who desires the best for people around him, so being able to celebrate your wins, is defo a honor. 
+        Happy graduation to you love, greater heights than this, more money to your pocket and fame to your name. ❤️❤️❤️`,
+        author: "Adeola"
+    },
+    {
+        msg: `Ayomide Alabi fondly called. 
+        It’s good to have him in my corner even if we don’t talk that much. My friendship with him has gone through various phases, like literally different phases. But I love what he stands for and how he would go out for people. Couldn’t be more proud of him bagging the degree. It’s better days ahead for you bro and the sky is just the beginning. Keep winning. 
+        Ayomide and I love you ❤️`,
+        author: "Ifeoluwa Makinde"
+    },
+    {
+        msg: `Ayomide Alabi my gee! You’re such a great friend. Kind, generous and just a vibe. I remember how you helped me passively and actively while I was struggling to do my dissertation. 
+        I’m so so happy for you that you’re finally done with your Masters. Welldone dear. Congratulations! 
+        See you at the top, my guy. 🤗😘 ❤️`,
+        author: "Seun Edema"
+    },
+    {
+        msg: `Hmmm Ayo,briefly Since I’ve known you you’ve been there for me at the some of the most crucial moment in my life back University days without you knowing and you always came through for me regardless what you were going through, I’ll always hold you dear to my heart My G,keep being the kindhearted person that you are. you deserve every good thing that has come your way or is coming your way. my only problem with you is that you don’t respect me enough knowing I’m the best in our CALL OF DUTY Team but no worries keep arguing records always tell the truth and stop shouting in our mic please it’s not fight 🤣 that being said As you celebrate your graduation, know your journey has just begun and your success has been written my Gee, Congratulations on your Graduation brother.`,
+        author: "Bash"
+    },
+    {
+        msg: `Ayomide Alabi 
+        To say I’m proud of you is an understatement
+        Congratulations Ayo, I am very happy for you.
+        Knowing how this masters started to seeing you having a Distinction is a beautiful thing to see.
+        I wish you the very best things of life`,
+        author: "Lamiji"
+    },
+    {
+        msg: `Knowing you is knowing true friendship. 
+        You have a kind heart and you go the extra mile for people even though you no dey listen to my advice sometimes and you still come back after you don mess up. I know you have a dream to be a captain and I pray your dream comes through. 
+        Dear Ayomide 🍑, here’s me wishing you a very big congratulations on the completion of your Msc.
+        Greater things await you ahead.`,
+        author: "Elushize"
+    },
+    {
+        msg: `Hey Alabi,  I have heard Good things about you,  Thank you for being a good friend to my man long before I came to the picture,  I pray things go well for you and you never stop being amazing.
+        Congratulations on bagging an Msc🎺🥳`,
+        author: "Pam"
+    },
+    {
+        msg: `First things first, Werey ni Ayo but all in all he’s a very good lad! Defo one of the best tbh! Known him for a bit but we started talking properly just recently and Omo it’s been straight up vibes, you can just tell he’s a good person and he’d be a very great person! I’m glad I’m part of his journey and I know we’re going to win big in this life! I wish him the very best of everything life has to offer and congratulations to him on achieving this milestone ❤️`,
+        author: "Akeeb"
+    }
+]
+
+export default wishes;
